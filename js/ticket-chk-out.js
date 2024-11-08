a3cca2b7b9dfa5bb6c167d1bf7d330a776c2007b
Manwaring <26ManwaringS@mydacc.org>
2024-11-08 10:22:40 -0500

    testcommit

--- js/ticket-chk-out.js
+++ js/ticket-chk-out.js
@@ -2,6 +2,7 @@ const plansSelectors = document.querySelectorAll(".plans");
 
 console.log("text", plansSelectors);
 
+
 plansSelectors.forEach((planSelector) => {
     planSelector.addEventListener("click", (event) => {
         // console.log(event);
@@ -9,21 +10,22 @@ plansSelectors.forEach((planSelector) => {
         if(event.target.checked && event.target.value === "general-ticket-select") {
          //console.log("general-ticket-select");
          const ticketCost = 25.00;
-         let ticketCount = document.getElementsByClassName(".ticket-count").value = 1;
+         let ticketCount = document.getElementById("ticket-count").value = 1;
          document.querySelector(".ticket-type-description").innerHTML = "General Ticket " + "x" + ticketCount;
          document.querySelector(".ticket-type-amount").innerHTML = "$25.00";
-         document.querySelector(".ticket-total").innerHTML = "US $" + ticketCost * ticketCount;
+         document.querySelector(".ticket-total").innerHTML = "US $" + (ticketCost * ticketCount);
         
 
-
+        
         }
         if(event.target.checked && event.target.value === "vip-ticket-select") {
           //console.log("vip-ticket-select");
           const ticketCost = 55.00;
-          let ticketCount = document.getElementsByClassName(".ticket-count").value = 1;
-          document.querySelector(".ticket-type-description").innerHTML = "VIP Ticket" + "x" + ticketCount;
+          let ticketCount1 = document.getElementById("ticket-count1").value = 1;
+
+          document.querySelector(".ticket-type-description").innerHTML = "VIP Ticket " + "x" + ticketCount1;
           document.querySelector(".ticket-type-amount").innerHTML = "$55.00";
-          document.querySelector(".ticket-total").innerHTML = "US $" + ticketCost * ticketCount;
+          document.querySelector(".ticket-total").innerHTML = "US $" + (ticketCost * ticketCount1);
         }
     });
 });
